8e99163dd7fba054badb527a8b401bc6709c71ba
mdorici <marianadorici@gmail.com>
2023-10-10 21:42:59 -0300

    Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="dsf-header">
+          <h2 className="title">DSFilter</h2>
+          <h3 className="products-count">nº produto(s)</h3>
+        </div>
+      </div>
+    </header>
+  );
+}
